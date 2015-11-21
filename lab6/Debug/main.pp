c2770b47accc3b48c62d121e0811e117606da722
Yosemit Xolalpa Rosales <xolalpay@unlv.nevada.edu>
2015-11-20 20:30:53 -0800

    lab06 - Low Power Modes

---
+++ lab6/Debug/main.pp
@@ -0,0 +1,37 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.1.71/utils/ustdlib.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/time.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.1.71/inc/hw_types.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.1.71/inc/hw_memmap.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.1.71/driverlib/sysctl.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.1.71/driverlib/pin_map.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.1.71/driverlib/debug.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.1.71/driverlib/hibernate.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.1.71/driverlib/gpio.h
+
+../main.c: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h: 
+C:/ti/TivaWare_C_Series-2.1.1.71/utils/ustdlib.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/time.h: 
+C:/ti/TivaWare_C_Series-2.1.1.71/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.1.71/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.1.71/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.1.71/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.1.71/driverlib/debug.h: 
+C:/ti/TivaWare_C_Series-2.1.1.71/driverlib/hibernate.h: 
+C:/ti/TivaWare_C_Series-2.1.1.71/driverlib/gpio.h: 
