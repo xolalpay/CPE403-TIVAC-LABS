7ae70dfdb0ff9a5f757f88068acf91b07b96cd6d
Yosemit Xolalpa Rosales <xolalpay@unlv.nevada.edu>
2015-11-22 15:57:17 -0800

    lab10-partE

--- lab10/main.c
+++ lab10/main.c
@@ -2,69 +2,78 @@
 #include <stdbool.h>
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
-#include "driverlib/debug.h"
+#include "driverlib/interrupt.h"
 #include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
 #include "grlib/grlib.h"
+#include "grlib/widget.h"
+#include "grlib/canvas.h"
+#include "grlib/pushbutton.h"
 #include "Kentec320x240x16_ssd2119_8bit.h"
+#include "touch.h"
 
 /**************************
 YOSEMIT XOLALPA ROSALES
 PROF. VENKI
 CPE403
-TIVAC-LAB10-C
+TIVAC-LAB10-partE
 **************************/
-extern const uint8_t g_pui8Image[];
-tContext sContext;
-tRectangle sRect;
+extern tCanvasWidget g_sBackground;
+extern tPushButtonWidget g_sPushBtn;
 
-void ClrScreen(void);
+void OnButtonPress(tWidget *pWidget);
 
+Canvas(g_sHeading, &g_sBackground, 0, &g_sPushBtn,
+       &g_sKentec320x240x16_SSD2119, 0, 0, 320, 23,
+       (CANVAS_STYLE_FILL | CANVAS_STYLE_OUTLINE | CANVAS_STYLE_TEXT),
+       ClrBlack, ClrWhite, ClrRed, g_psFontCm20, "LED Control", 0, 0);
+
+Canvas(g_sBackground, WIDGET_ROOT, 0, &g_sHeading,
+       &g_sKentec320x240x16_SSD2119, 0, 23, 320, (240 - 23),
+       CANVAS_STYLE_FILL, ClrBlack, 0, 0, 0, 0, 0, 0);
+
+RectangularButton(g_sPushBtn, &g_sHeading, 0, 0,
+                  &g_sKentec320x240x16_SSD2119, 60, 60, 200, 40,
+                  (PB_STYLE_OUTLINE | PB_STYLE_TEXT_OPAQUE | PB_STYLE_TEXT |
+                   PB_STYLE_FILL), ClrGray, ClrWhite, ClrRed, ClrRed,
+                   g_psFontCmss22b, "Toggle red LED", 0, 0, 0, 0, OnButtonPress);
+
+bool g_RedLedOn = false;
+
+void OnButtonPress(tWidget *pWidget)
+{
+    g_RedLedOn = !g_RedLedOn;
+
+    if(g_RedLedOn)
+    {
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0x02);
+    }
+    else
+    {
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0x00);
+    }
+}
 int main(void)
 {
-   SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);
 
-   Kentec320x240x16_SSD2119Init();
-   GrContextInit(&sContext, &g_sKentec320x240x16_SSD2119);
-   ClrScreen();
+    SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
 
-   GrImageDraw(&sContext, g_pui8Image, 0, 0);
-   GrFlush(&sContext);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x00);
 
-   SysCtlDelay(SysCtlClockGet());
-   // Later lab steps go between here
+    Kentec320x240x16_SSD2119Init();
 
-   ClrScreen();
+    TouchScreenInit();
 
-   sRect.i16XMin = 1;
-   sRect.i16YMin = 1;
-   sRect.i16XMax = 318;
-   sRect.i16YMax = 238;
-   GrContextForegroundSet(&sContext, ClrRed);
-   GrContextFontSet(&sContext, &g_sFontCmss30b);
-   GrStringDraw(&sContext, "CPE403", -1, 110, 2, 0);
-   GrStringDraw(&sContext, "LAB 10", -1, 80, 32, 0);
-   GrStringDraw(&sContext, "GRAPHICS", -1, 100, 62, 0);
-   GrStringDraw(&sContext, "LIBRARY", -1, 135, 92, 0);
-   GrContextForegroundSet(&sContext, ClrWhite);
-   GrRectDraw(&sContext, &sRect);
-   GrFlush(&sContext);
+    TouchScreenCallbackSet(WidgetPointerMessage);
 
-   SysCtlDelay(SysCtlClockGet());
+    WidgetAdd(WIDGET_ROOT, (tWidget *)&g_sBackground);
 
-   // and here
-   ClrScreen();
-   while(1)
-   {
-   }
-}
+    WidgetPaint(WIDGET_ROOT);
 
-void ClrScreen()
-{
-   sRect.i16XMin = 0;
-   sRect.i16YMin = 0;
-   sRect.i16XMax = 319;
-   sRect.i16YMax = 239;
-   GrContextForegroundSet(&sContext, ClrBlack);
-   GrRectFill(&sContext, &sRect);
-   GrFlush(&sContext);
+    while(1)
+    {
+        WidgetMessageQueueProcess();
+    }
 }
